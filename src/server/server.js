f9cbbb7358f98b0d561c4b59896d55acb8b1e7bf
Tudor Tacal <tudortacal@gmail.com>
2018-07-08 00:39:42 +0100

    Run app on Heroku

--- src/server/server.js
+++ src/server/server.js
@@ -2,8 +2,9 @@ import express from 'express';
 
 const app = express();
 
+const PORT = process.env.PORT || '3000';
 app.get('/', (req, res) => {
   res.send('Hello World');
 })
 
-app.listen(3000, () => console.log('App starting on port 3000'));
+app.listen(3000, () => console.log(`App starting on port ${PORT}`));
