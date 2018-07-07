bb3cbb13cb6a6f8f90cd86e39b88cff0cec7fd5e
Tudor Tacal <tudortacal@gmail.com>
2018-07-08 00:47:37 +0100

    App listen to PORT constant instead of 3000

--- src/server/server.js
+++ src/server/server.js
@@ -2,9 +2,9 @@ import express from 'express';
 
 const app = express();
 
-const PORT = process.env.PORT || '3000';
+const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.send('Hello World');
 })
 
-app.listen(3000, () => console.log(`App starting on port ${PORT}`));
+app.listen(PORT, () => console.log(`App starting on port ${PORT}`));
