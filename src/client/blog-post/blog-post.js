8ff217f676a33a6730bf2af89ccd86d53e9efdbd
Tudor Tacal <tudortacal@gmail.com>
2018-08-28 09:47:07 +0100

    Add a minimum of content to the first post

--- src/client/blog-post/blog-post.js
+++ src/client/blog-post/blog-post.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 const BlogPost = () => {
   return(
-    <h1>This shall be the infamous blog post page</h1>
+    <React.Fragment>
+      <h1>I just finished the server and client side routing.</h1>
+      <h1>Next, I'm planning to style this page and write the content.</h1>
+      <h1>So please bare with me :)</h1>
+      <small><i>Works in progress</i></small>
+    </React.Fragment>
   )
 };
 
