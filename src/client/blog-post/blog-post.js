f6253d09a970e791b67eb79d97cdaf1fbfd36e52
Tudor Tacal <tudortacal@gmail.com>
2018-08-29 23:52:33 +0100

    Update BlogPost text

--- src/client/blog-post/blog-post.js
+++ src/client/blog-post/blog-post.js
@@ -3,9 +3,9 @@ import React from 'react';
 const BlogPost = () => {
   return(
     <React.Fragment>
-      <h1>I just finished the server and client side routing.</h1>
-      <h1>Next, I'm planning to style this page and write the content.</h1>
-      <h1>So please bare with me :)</h1>
+      <h1>Server and client side routing - done</h1>
+      <h1>Styling the page - in progress.</h1>
+      <h1>Adding content - in another life :)</h1>
       <small><i>Works in progress</i></small>
     </React.Fragment>
   )
