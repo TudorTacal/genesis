468c15b68ced066aea4226a93302c31ae1954d7b
Tudor Tacal <tudortacal@gmail.com>
2018-10-03 22:00:06 +0100

    Auth0 - Render auth0lock sign-in link
    - create Header component
    - find a way to extract the information from the access_token
    - think if you actually need a id_token using a JWT
    - learn the difference between the 2
    - read the articles on auth0 website

--- src/client/App.js
+++ src/client/App.js
@@ -4,12 +4,14 @@ import { Route, Switch } from 'react-router-dom';
 import HomePage from './home-page/home-page';
 import BlogPost from './blog-post/blog-post';
 import NotFound from './not-found/not-found';
+import Header from './header/header';
 import posts from '../model/posts';
 
 class App extends React.Component {
   render() {
     return (
       <React.Fragment>
+        <Header />
         <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/posts/:id" render={(props) => (
---
+++ src/client/header/header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+import Auth0Lock from 'auth0-lock';
+
+class Header extends React.Component {
+  showLock = () => {
+    // Show the Auth0Lock widget
+    this.lock.show();
+  };
+  componentDidMount() {
+    this.lock = new Auth0Lock(
+      process.env.AUTH0_CLIENT_ID,
+      process.env.AUTH0_CLIENT_DOMAIN,
+    );
+  }
+  render () {
+    return(
+      <a onClick={this.showLock}>Sign In</a>  
+    )
+  }
+};
+
+export default Header;
