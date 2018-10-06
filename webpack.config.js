b402b5be39f7017275e74a61ec663a93024ff048
Tudor Tacal <tudortacal@gmail.com>
2018-10-06 22:20:15 +0100

    Stringify process.env variables
    - the problem was the config varbiables
    got deleted from Heroku

--- webpack.config.js
+++ webpack.config.js
@@ -41,8 +41,8 @@ module.exports = {
     new CleanWebpackPlugin(['dist']),
     new Dotenv(),
     new webpack.DefinePlugin({
-      'process.env.AUTH0_CLIENT_ID': process.env.AUTH0_CLIENT_ID,
-      'process.env.AUTH0_CLIENT_DOMAIN': process.env.AUTH0_CLIENT_DOMAIN,
+      'process.env.AUTH0_CLIENT_ID': JSON.stringify(process.env.AUTH0_CLIENT_ID),
+      'process.env.AUTH0_CLIENT_DOMAIN': JSON.stringify(process.env.AUTH0_CLIENT_DOMAIN),
       'process.env.variable': JSON.stringify('variable'), 
     }),    
   ],
